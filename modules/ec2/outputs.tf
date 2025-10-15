b73e8390effadbedbec1901066ef02179890797b
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2025-10-15 17:24:21 +0100

    Automate CloudWatch Agent installation for EC2 instances by default

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -2,4 +2,11 @@ output "public_ips" {
   value = { for k, v in aws_instance.this : k => v.public_ip }
 }
 
+output "ec2_instance_ids" {
+  value = { for k, v in aws_instance.this : k => v.id }
+}
 
+output "private_key_pem" {
+  value     = tls_private_key.ec2_key[0].private_key_pem
+  sensitive = true
+}
