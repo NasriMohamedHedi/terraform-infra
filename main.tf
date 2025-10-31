748c0382dfefa37512140ccf6fba59f5762128c2
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2025-10-31 14:57:35 +0100

    fix: add -upgrade to terraform init

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
     }
   }
 }
