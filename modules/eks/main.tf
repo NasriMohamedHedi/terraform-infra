2a83d1e4cc9adcfe58ae3c39a85e166bb8ddca12
NasriMohamedHedi <nasrimohamedhedi0@gmail.com>
2025-10-31 14:47:28 +0100

    fix: delete outputs.tf + add required_providers in module

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
 # Generate unique suffix
 resource "random_id" "unique_suffix" {
   byte_length = 4
